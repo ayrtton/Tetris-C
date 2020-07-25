aeca26221936bb32c96a9aa488b019fe322a917f
ayrtton <46231350+ayrtton@users.noreply.github.com>
2020-07-25 18:09:50 -0300

    Values
    Developing the logic of the game.

--- main.c
+++ main.c
@@ -16,6 +16,7 @@ int speed = 200;
 void draw_screen();
 void time_stream();
 void deploy_block();
+void set_values();
 
 int main()
 {
@@ -113,3 +114,63 @@ void deploy_block() {
     }
 
 }
+
+void set_values() {
+    int i, j;
+
+    for(i=0; i < HEIGHT; i++)
+        for(j=0; j < WIDTH; j++)
+            if(board[i][j] != 2)
+                board[i][j] = 0;
+
+    switch(position) {
+
+        case 0:
+            for(i = 0; i<2; i++) {
+                for(j = 0; j<4; j++) {
+                    piece[i][j].y = j + y_pos;
+                    piece[i][j].x = i + x_pos;
+                }
+            }
+            break;
+
+        case 1:
+            for(i=0; i<2; i++) {
+                for(j=0; j<4; j++) {
+                    piece[i][j].y = 2-i + y_pos;
+                    piece[i][j].x = j + x_pos;
+                }
+            }
+            break;
+
+        case 2:
+            for(i=0; i<2; i++) {
+                for(j=0; j<4; j++) {
+                    piece[i][j].y = 2-j + y_pos;
+                    piece[i][j].x = 2-i + x_pos;
+                }
+            }
+            break;
+
+        case 3:
+            for(i=0; i<2; i++) {
+                for(j=0; j<4; j++) {
+                    piece[i][j].y = i + y_pos;
+                    piece[i][j].x = 2-j + x_pos;
+                }
+            }
+            break;
+
+        default:
+            break;
+
+    }
+
+    for(i=0; i<2; i++) {
+        for(j=0; j<4; j++) {
+            if(piece[i][j].value == 1)
+                board[piece[i][j].x][piece[i][j].y] = 1;
+        }
+    }
+}
+
